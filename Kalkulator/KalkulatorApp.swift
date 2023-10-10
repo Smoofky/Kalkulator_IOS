edb7da9500e2b52e8bb796d1b41e171d7f8a9c34
Kamil Rytel <rytelkamil00@gmail.com>
2023-10-10 10:10:21 +0200

    Initial Commit

---
+++ Kalkulator/KalkulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulatorApp.swift
+//  Kalkulator
+//
+//  Created by student on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KalkulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
